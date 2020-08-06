ae59b44326773a8db8bbc3fa7d05e6a4aca851d4
HansBlackCat <jeremy3599@snu.ac.kr>
2020-08-06 15:25:20 +0900

    1

--- APSS/FESTIVAL.cpp
+++ APSS/FESTIVAL.cpp
@@ -2,17 +2,39 @@
 // Created by HansBlackCat on 8/5/20.
 //
 
+#include <iostream>
+#include <algorithm>
 #include <vector>
-#include <string>
-#include <cstdio>
-
+using namespace std;
 
 int main() {
-    const int MAX_SIZE = (int)1e+6;
-    char ipt[MAX_SIZE];
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
 
+    int C;
+    cin >> C;
 
-    scanf("%[^\t]", ipt);
-    printf
+    while (C--) {
+        int N,L;
+        cin >> N >> L;
 
+        vector<int> cost(N);
+        for (int i = 0; i < N; i++) cin >> cost[i];
+
+        double avg = 1e20;
+        for (int i = 0, j; i < N - L + 1; i++) {
+            int sum = 0;
+            int count = 1;
+
+            for (j = i; j < i + L - 1; j++, count++) {
+                sum += cost[j];
+            }
+
+            for (; j < N; j++, count++) {
+                sum += cost[j];
+                avg = min(avg, (double)sum / count);
+            }
+        }
+        printf("%.10lf\n", avg);
+    }
 }
---
+++ APSS/_sort_test.cpp
@@ -0,0 +1,80 @@
+//
+// Created by HansBlackCat on 8/6/20.
+//
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void selection_sort(vector<int>& A) {
+    for (int i = 0; i < A.size(); i++) {
+        int INDEX = i;
+        for (int j = i+1; j < A.size(); j++) {
+            if (A[INDEX] > A[j]) {
+                INDEX = j;
+            }
+        }
+        swap(A[i], A[INDEX]);
+    }
+}
+
+void insertion_sort(vector<int>& A) {
+    for (int i = 0; i < A.size(); i++) {
+        int j = i;
+        while (j > 0 && A[j-1] > A[j]) {
+            swap(A[j-1], A[j]);
+            --j;
+        }
+    }
+}
+
+// O(N^2)
+const int MIN = numeric_limits<int>::min();
+int better_max_sum(const vector<int>& A) {
+    int N = A.size();
+    int ret = MIN;
+
+    for (int i = 0; i < N; i++) {
+        int sum = 0;
+        for (int j = i; j < N; j++) {
+            sum += A[j];
+            ret = min(sum, ret);
+        }
+    }
+    return ret;
+}
+
+// O(NlogN)
+int fast_max_sum(const vector<int>& A, int lo, int hi) {
+    if (lo == hi) return A[lo];
+
+    int mid = (lo + hi) >> 1;
+
+    int left = MIN, right = MIN, sum = 0;
+    for (int i = mid; i >= lo; --i) {
+        sum += A[i];
+        left = max(left, sum);
+    }
+
+    sum = 0;
+    for (int j = mid+1; j <= hi; ++j) {
+        sum += A[j];
+        right = max(right, sum);
+    }
+
+    int single = max(fast_max_sum(A, lo, mid), fast_max_sum(A, mid+1, hi));
+
+    return max(left + right, single);
+}
+
+// O(N)
+int fastest_max_sum(const vector<int>& A) {
+    int N = A.size(), ret = MIN, psum = 0;
+
+    for (int i = 0; i < N; i++) {
+        psum = max(psum, 0) + A[i];
+        ret = max(psum, ret);
+    }
+
+    return ret;
+}
